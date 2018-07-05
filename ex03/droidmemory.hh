1ac56bf67c93711e7f6d614c94d0b429a975e56c
Watson <you@example.com>
2018-07-05 13:04:29 +0800

    my commit

---
+++ ex03/droidmemory.hh
@@ -0,0 +1,42 @@
+#ifndef DROIDMEMORY_H_
+#define DROIDMEMORY_H_
+
+#include <iostream>
+
+class DroidMemory
+{
+public:
+    DroidMemory();
+    virtual ~DroidMemory();
+
+    size_t getFingerPrint() const;
+    void setFingerPrint(size_t const value);
+    size_t getExp() const;
+    void setExp(size_t const value);
+
+    DroidMemory& operator<<(DroidMemory const& other);
+    DroidMemory& operator>>(DroidMemory& other) const;
+    DroidMemory& operator+=(DroidMemory const& other);
+    DroidMemory& operator+=(size_t const& val);
+    DroidMemory operator+(DroidMemory const& other) const;
+    DroidMemory operator+(size_t const val) const;
+    bool operator==(DroidMemory const& other) const;
+    bool operator!=(DroidMemory const& other) const;
+    bool operator<(DroidMemory const& other) const;
+    bool operator<(size_t other) const;
+    bool operator>(DroidMemory const& other) const;
+    bool operator>(size_t other) const;
+    bool operator<=(DroidMemory const& other) const;
+    bool operator<=(size_t other) const;
+    bool operator>=(DroidMemory const& other) const;
+    bool operator>=(size_t other) const;
+
+private:
+    size_t _fingerPrint;
+    size_t _exp;
+};
+
+std::ostream& operator<<(std::ostream& stream, DroidMemory const& memory);
+
+
+#endif /* !DROIDMEMORY_H_ */
